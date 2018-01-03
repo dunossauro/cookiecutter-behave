68000d44c88797d872d8e796017c7bfa5ba75fa5
z4r4tu5tr4 <mendesxeduardo@gmail.com>
2018-01-03 18:41:48 -0200

    modules and auxiliar

---
+++ {{cookiecutter.project_name}}-bdd/{{cookiecutter.project_name}}/auxiliar/constants.py
@@ -0,0 +1,12 @@
+"""When you write your constants to help you."""
+
+
+class Constants(object):
+    """
+    Constants class.
+
+    You can use this class like a monkey path class for don't generate
+        side effect using context.
+    """
+    def __init__(self):
+        exe_path = r'C:\\hello.exe'
---
+++ {{cookiecutter.project_name}}-bdd/{{cookiecutter.project_name}}/modules/mymodule.py
@@ -0,0 +1,8 @@
+"""Module example.
+
+You can use modules for simplify the functions calls in your tests.
+"""
+
+
+def add(x, y):
+    return x + y
